f1a2a07a73d481aa0b8e21d76d8e733d10ea2a06
oxlzlo <yeojin58@naver.com>
2024-12-17 17:13:27 +0900

    design: 모집 직군 카드 디자인

---
+++ src/components/Main/OccupationCard.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const OccupationCard = () => {
+  return (
+    <OccupationCardFrame>
+      <SelectedCard>프론트엔드</SelectedCard>
+      <SelectedCard>백엔드</SelectedCard>
+      <SelectedCard>디자이너</SelectedCard>
+      <SelectedCard>기획자</SelectedCard>
+      <SelectedCard>데브옵스</SelectedCard>
+    </OccupationCardFrame>
+  );
+};
+
+export default OccupationCard;
+
+const OccupationCardFrame = styled.div`
+  display: flex;
+  gap: 4px;
+  width: 512px;
+  height: 32px;
+  margin-left: auto;
+`;
+
+const SelectedCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20px;
+  padding: 6px 12px;
+  border: 1px solid #bababb;
+  border-radius: 6px;
+  font-size: 14px;
+  font-weight: 500;
+`;
