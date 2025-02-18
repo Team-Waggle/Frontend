5d46ea496c5d970973620aabff0cb94bb51d7cdc
Nam Min Ki <nam960216@gmail.com>
2025-02-18 22:19:24 +0900

    fix: no-unused-vars 삭제

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,41 +4,39 @@ module.exports = {
     browser: true, // 브라우저 환경을 사용한다.
     node: true, // Node.js 환경을 사용한다.
   },
-  parser: "@typescript-eslint/parser", // TypeScript 사용시, JavaScript 사용시 "@babel/eslint-parser" 사용
+  parser: '@typescript-eslint/parser', // TypeScript 사용시, JavaScript 사용시 "@babel/eslint-parser" 사용
   parserOptions: {
     ecmaVersion: 2022, // 가장 최근에 지원되는 버전을 사용
-    sourceType: "module", // module로 설정된 경우에는 import 구문을 사용하는 것이 유효
+    sourceType: 'module', // module로 설정된 경우에는 import 구문을 사용하는 것이 유효
     ecmaFeatures: {
       jsx: true, // jsx 활성화 *jsx 구문 지원과 react 지원은 틀리므로 react를 사용하는 경우 eslint-plugin-react 사용
     },
   },
-  plugins: ["@typescript-eslint", "prettier", "react-refresh"],
+  plugins: ['@typescript-eslint', 'prettier', 'react-refresh'],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
   extends: [
-    "eslint:recommended", //사용하지 않는 변수와 같은 코드 품질 검사는 ESLint 추천 규칙 사용
-    "plugin:react/recommended", // ESLint 설정에서 React 플러그인의 권장 규칙 세트를 활성화
-    "plugin:@typescript-eslint/recommended", // TypeScript 사용 시 적용
-    "plugin:prettier/recommended", // eslint-config-prettier를 실제로 활성화시켜서 중복되는 룰을 끄도록 하는 설정.
+    'eslint:recommended', //사용하지 않는 변수와 같은 코드 품질 검사는 ESLint 추천 규칙 사용
+    'plugin:react/recommended', // ESLint 설정에서 React 플러그인의 권장 규칙 세트를 활성화
+    'plugin:@typescript-eslint/recommended', // TypeScript 사용 시 적용
+    'plugin:prettier/recommended', // eslint-config-prettier를 실제로 활성화시켜서 중복되는 룰을 끄도록 하는 설정.
   ],
   rules: {
-    "no-var": "error", // var 허용 금지
-    "no-unused-vars": ["off"],
-    "@typescript-eslint/no-unused-vars": "warn",
-    "react/react-in-jsx-scope": "off", //import React from 'react' 없이 사용 가능한 설정
-    "react-refresh/only-export-components": [
-      "warn",
+    'no-var': 'error', // var 허용 금지
+    '@typescript-eslint/no-unused-vars': 'warn', // 타입 스크립트 사용 시 인터페이스 충돌 방지
+    'react/react-in-jsx-scope': 'off', //import React from 'react' 없이 사용 가능한 설정
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
     // "no-empty-static-block": "error", // 빈 함수를 허용하지 않음.
     // "unused-imports/no-unused-imports": "error", // 사용되지 않는 import 문을 검사하여 코드베이스에서 불필요한 import 문을 감지
     // "require-await": "error", // async 사용 시 await 사용 필수
-    // "@typescript-eslint/no-unused-vars": "off", // 타입 스크립트 사용 시 인터페이스 충돌 방지
     // "react-refresh/only-export-components": "warn", //   React.memo, React.forwardRef, React.lazy 등과 같은 React의 고급 기능을 사용하지 않는 경우에 발생할 수 있는 경고를 나타냅니다. 이러한 고급 기능을 사용할 때 Fast Refresh와 호환성 문제가 발생할 수 있으므로, 이 규칙은 개발자에게 해당 부분을 검토
     // "react-hooks/rules-of-hooks": "error", // React Hooks 사용에 관련된 규칙 적용
     // "react-hooks/exhaustive-deps": "warn", // React Hooks의 useEffect 함수에서 의존성 배열을 검사
