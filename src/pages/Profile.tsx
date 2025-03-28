6485c4323f6fe7b2537e653c82f88815d1857684
예은 <3_14km@naver.com>
2025-03-28 20:48:24 +0900

    feat: Profile 페이지 생성

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Profile = () => {
+    return (
+        <MainWrapper>
+
+
+        </MainWrapper>
+    );
+};
+
+export default Profile;
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1200px;
+`;
