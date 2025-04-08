1f379f41a1619e338e30320fd651103a725b9c65
예은 <3_14km@naver.com>
2025-04-09 04:18:51 +0900

    design: StyledInput 컴포넌트 글자 크기 수정
    Profile 설정 페이지에서 쓰이는 StyledInput 글자 크기 수정

--- src/components/Profile/StyledInput.tsx
+++ src/components/Profile/StyledInput.tsx
@@ -4,7 +4,7 @@ const StyledInput = ({ className, style, ...rest }: StyledInputProps) => {
   return (
     <input
       {...rest}
-      className={`w-full h-[var(---46-,46px)] pr-[8px] pl-[18px] py-0 items-center self-stretch border rounded-[8px] border-[#c4c4c6] ${className ?? ''}`}
+      className={`w-full h-[var(---46-,46px)] pr-[8px] pl-[18px] py-0 items-center self-stretch text-[16px] border rounded-[8px] border-[#c4c4c6] ${className ?? ''}`}
       style={{
         paddingRight: "var(--46-, 8px)",
         paddingLeft: "var(--46-, 18px)",
