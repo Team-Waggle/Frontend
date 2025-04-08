aeff4aac12c092d5632dbf1e42bbbe2545d6ee7f
예은 <3_14km@naver.com>
2025-04-09 04:20:35 +0900

    feat, design: Profile 설정 페이지
    1. 일부 변경된 Dropdown 관련해서 설정 변동
    2. 전체적인 퍼블리싱 작업

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -10,13 +10,55 @@ import StyledInput from "../components/Profile/StyledInput";
 import IndustrySelect from "../components/Profile/IndustrySelect";
 import DaySelect from "../components/Profile/DaySelect";
 
+import Dropdown from "../components/Dropdown";
+import DropdownC from "../components/DropdownC";
+import CalendarDropdown from "../components/CalendarDropdown";
+import DropdownWithLabel from "../components/DropdownWithLabel";
+
+// 1. 드롭다운 2. 팀플 성향 그리드 3. 자기소개 4. 사용 스킬 자동 완성
+// 그 다음으로 Active, Hover 작업
+// 최대 5개 선택 가능 텍스트 size / bold 변경
+
+// 직무 및 경력 - 드롭다운
+// 관심 산업 분야 - 최대 5개 선택 가능 텍스트 size, bold
+// 선호 시간 - 드롭다운
+// 선호 진행방식 및 지역 - 드롭다운
+// 팀플 성향 - 그리드, 최대 5개 선택 가능 텍스트 size, bold
+// 자기소개 - textarea
+// 링크 - input, 드롭다운
+
 const Profile = () => {
-    // 수정 (공부하기)
+    const jobs = [
+        "IOS", "기획자", "데브옵스", "디자이너", "마케터",
+    ];
+
     const industryList = [
         "건강", "건설", "금융", "교육", "문화/예술",
         "미디어/광고", "부동산", "뷰티/패션", "소셜네트워크",
         "의료/헬스케어", "여행", "육아/출산", "엔터테인먼트",
-        "이커머스", "인테리어", "종교", "판매/유통", "기타"
+        "이커머스", "인테리어", "종교", "판매/유통", "기타",
+    ];
+
+    const workExperience = [
+        "0년차", "1~3년차", "4~6년차", "7~10년차", "10년차 이상"
+    ];
+
+    const preferredTime = [
+        "오전 (6:00~12:00)", "오후 (12:00~18:00)", "저녁 (18:00~22:00)",
+        "심야 (22:00~02:00)", "새벽 (02:00~06:00)"
+    ];
+
+    const onOff = [
+        "온라인만", "오프라인만", "온/오프라인"
+    ];
+
+    const region = [
+        "서울특별시", "경기도", "인천광역시", "강원특별자치도", "대전광역시", "추가예정"
+    ];
+
+    // 이미지 추가
+    const site = [
+        "Branch story", "Dribbble", "GitHub", "Instagram", "Notion", "추가예정"
     ];
 
     // 수정 (공부하기)
@@ -28,12 +70,12 @@ const Profile = () => {
 
     // 수정 (공부하기)
     const industryChunks = chunk(industryList, 9);
-    
+
     return (
         <div className="flex flex-col items-center w-[1200px]">
             <div className="mt-[42px] mb-[20px]"> <ProfileSetting /> </div>
 
-            <div className="max-w-[734px] flex flex-col items-start gap-[26px]">
+            <div className="w-[734px] flex flex-col items-start gap-[26px]">
                 {/* 닉네임 */}
                 <div className="flex flex-col gap-[8px] w-full">
                     <FormLabel> 닉네임 <RequiredIcon /> </FormLabel>
@@ -48,31 +90,29 @@ const Profile = () => {
                 </div>
 
                 {/* 직무 및 경력 */}
-                <div className="flex flex-col gap-[8px] w-full">
+                <div className="flex items-start flex-col self-stretch gap-[8px] w-full">
                     <FormLabel> 직무 및 경력 <RequiredIcon /> </FormLabel>
-                    <div className="flex gap-[6px]">
-                        <select>
-                            <option> IOS </option>
-                            <option> 기획자 </option>
-                            <option> 데브옵스 </option>
-                            <option> 디자이너 </option>
-                            <option> 마케터 </option>
-                        </select>
-                        <select>
-                            <option> 0년차 </option>
-                            <option> 1~3년차 </option>
-                            <option> 4~6년차 </option>
-                            <option> 7~10년차 </option>
-                            <option> 10년차 이상 </option>
-                        </select>
-                        <PlusIcon />
+                    <div className="flex items-center gap-[6px]">
+                        <div className="flex w-[642px] items-center gap-[18px]">
+                            <DropdownC
+                                items={jobs}
+                                title="직무 선택"
+                            />
+                            <DropdownC
+                                items={workExperience}
+                                title="경력 선택"
+                            />
+                        </div>
+                        <div className="flex w-[40px] h-[40px] justify-center items-center gap-[10px]">
+                            <PlusIcon />
+                        </div>
                     </div>
                 </div>
 
                 {/* 관심 산업 분야 */}
                 <div className="flex flex-col gap-[8px] w-full">
                     <FormLabel> 관심 산업 분야 <RequiredIcon /> 최대 5개 선택 가능 </FormLabel>
-                    <div className="flex flex-col gap-[10px]">
+                    <div className="flex flex-col items-start content-start self-stretch flex-wrap gap-y-[10px] gap-x-[5px] pb-[10px]">
                         {industryChunks.map((row, rowIndex) => (
                             <div key={rowIndex} className="flex gap-[5px]">
                                 {row.map((item) => (
@@ -93,7 +133,7 @@ const Profile = () => {
                 <div className="flex flex-col gap-[8px] w-full">
                     <FormLabel> 선호 요일 및 시간 </FormLabel>
                     <div className="flex gap-[16px] items-center self-stretch">
-                        <div className="flex flex-[1_0_0%] justify-between">
+                        <div className="flex flex-[1_0_0%] items-center justify-between rounded-[8px]">
                             <DaySelect> 월 </DaySelect>
                             <DaySelect> 화 </DaySelect>
                             <DaySelect> 수 </DaySelect>
@@ -102,56 +142,97 @@ const Profile = () => {
                             <DaySelect> 토 </DaySelect>
                             <DaySelect> 일 </DaySelect>
                         </div>
-                        <select>
-                            <option> 선호 시간 </option>
-                        </select>
+                        <div className="w-[358px]">
+                            <DropdownC
+                                items={preferredTime}
+                                title="선호 시간"
+                            />
+                        </div>
                     </div>
                 </div>
 
                 {/* 선호 진행방식 및 지역 */}
                 <div className="flex flex-col gap-[8px] w-full">
                     <FormLabel> 선호 진행방식 및 지역 <RequiredIcon /> </FormLabel>
-                    <div>
-                        <select>
-                            <option> 진행 방식 </option>
-                        </select>
-                        <select>
-                            <option> 지역 선택 </option>
-                        </select>
+                    <div className="flex items-center gap-[18px] self-stretch">
+                        <DropdownC
+                            items={onOff}
+                            title="진행 방식"
+                        />
+                        <DropdownC
+                            items={region}
+                            title="지역 선택"
+                        />
                     </div>
                 </div>
 
-                {/* 팀플 성향 */}
-                <div className="flex flex-col gap-[8px] w-full">
+                {/* 팀플 성향 + 유지보수 쉽게 리팩토링 해야함 */}
+                <div className="flex flex-col flex-start self-stretch gap-[20px] w-full">
                     <FormLabel> 팀플 성향 <RequiredIcon /> 최대 5개 선택 가능 </FormLabel>
-                    <div>
-                        <div className="flex">
-                            <div> 소통 스타일 </div>
-                            <div> 협업 성향 </div>
-                            <div> 말하는 방식 </div>
-                            <div> 문제 해결방식 </div>
-                            <div> MBTI </div>
+                    <div className="flex flex-col items-start gap-[14px] slef-stretch">
+                        <div className="flex justify-between items-start self-stretch">
+                            <div className="flex w-[140px] h-[32px] justify-center items-center rounded-[4px] bg-[#D9E8FF]
+                            text-[#000] text-[13px] font-[500] leading-[150%] text-center"> 
+                                소통 스타일
+                            </div>
+                            <div className="flex w-[140px] h-[32px] justify-center items-center rounded-[4px] bg-[#fff]
+                            text-[#949598] text-[13px] font-[500] leading-[150%] text-center"> 
+                                협업 성향
+                            </div>
+                            <div className="flex w-[140px] h-[32px] justify-center items-center rounded-[4px] bg-[#fff]
+                            text-[#949598] text-[13px] font-[500] leading-[150%] text-center"> 
+                                말하는 방식
+                            </div>
+                            <div className="flex w-[140px] h-[32px] justify-center items-center rounded-[4px] bg-[#fff]
+                            text-[#949598] text-[13px] font-[500] leading-[150%] text-center"> 
+                                문제 해결방식
+                            </div>
+                            <div className="flex w-[140px] h-[32px] justify-center items-center rounded-[4px] bg-[#fff]
+                            text-[#949598] text-[13px] font-[500] leading-[150%] text-center"> 
+                                MBTI
+                            </div>
                         </div>
-                        <div className="flex">
+                        {/* 형식 변경 */}
+                        <div className="flex justify-between items-start content-start self-stretch flex-wrap">
                             <div>
-                                <div> 1-1 </div>
-                                <div> 1-2</div>
+                                <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF] mb-[10px]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 1-1 </div>
+                                <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 1-2 </div>
                             </div>
                             <div>
-                                <div> 2-1 </div>
-                                <div> 2-2</div>
+                            <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF] mb-[10px]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 2-1 </div>
+                                <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 2-2 </div>
                             </div>
                             <div>
-                                <div> 3-1 </div>
-                                <div> 3-2</div>
+                            <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF] mb-[10px]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 3-1 </div>
+                                <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 3-2 </div>
                             </div>
                             <div>
-                                <div> 4-1 </div>
-                                <div> 4-2</div>
+                            <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF] mb-[10px]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 4-1 </div>
+                                <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 4-2 </div>
                             </div>
                             <div>
-                                <div> 5-1 </div>
-                                <div> 5-2</div>
+                            <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF] mb-[10px]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 5-1 </div>
+                                <div className="flex w-[140px] h-[80px] justify-center items-center
+                                rounded-[10px] border border-[#C4C4C6] bg-[#FFFFFF]
+                                text-center text-[#949598] text-[13px] font-[500] leading-[150%]"> 5-2 </div>
                             </div>
                         </div>
 
@@ -161,19 +242,34 @@ const Profile = () => {
                 {/* 자기 소개 */}
                 <div className="flex flex-col gap-[8px] w-full">
                     <FormLabel> 자기 소개 </FormLabel>
-                    <textarea placeholder="나는 어떤 사람인지, 어떤 프로젝트를 찾고 있는지, 간단한 인사도 좋아요!" />
+                    <div className="flex flex-col items-start gap-[6px] self-stretch">
+                        <textarea
+                        maxLength={300}
+                        className="flex h-[212px] pt-[16px] pr-[18px] pb-[20px] pl-[18px] items-start gap-[10px] self-stretch
+                        rounded-[8px] border border-[#C4C4C6] bg-[#FFFFFF]"
+                        placeholder="나는 어떤 사람인지, 어떤 프로젝트를 찾고 있는지, 간단한 인사도 좋아요!" />
+                        <div className="self-stretch text-[#949598] text-[14px] font-[500] leading-[168%] text-right"> N / 300 </div>
+                    </div>
                 </div>
 
                 {/* 링크 */}
-                <div className="flex flex-col gap-[8px] w-full">
+                <div className="flex flex-col items-start self-stretch gap-[8px] w-full">
                     <FormLabel> 링크 </FormLabel>
-                    <div>
-                        <input className="h-[var(---46-,46px)] pr-[8px] pl-[18px] py-0 items-center self-stretch border rounded-[8px] border-[#c4c4c6]"
-                        placeholder="주소" />
-                        <select>
-                            <option> 사이트 </option>
-                        </select>
-                        <PlusIcon />
+
+                    {/* 아래 CSS 수정 필요. dropdown w-[333px] 이 안 먹음 임시방편으로 주소Input에 291px 먹임*/}
+                    <div className="flex items-center gap-[6px]">
+                        <div className="flex w-[642px] items-center gap-[18px]">
+                            <div className="w-[291px]"> <StyledInput placeholder="주소" /> </div>
+                            <div className="flex w-[333px]">
+                                <DropdownC
+                                    items={site}
+                                    title="사이트"
+                                />
+                            </div>
+                        </div>
+                        <div className="flex w-[40px] h-[40px] justify-center items-center gap-[10px]">
+                            <PlusIcon />
+                        </div>
                     </div>
                 </div>
             </div>
