0e82c679ff06ab828677a7d7a77956686109bdbb
예은 <3_14km@naver.com>
2025-04-15 16:55:50 +0900

    chore: tailwind/vite관련 오류 제거, 파일 확장자 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [react(), svgr(), tailwindcss(),]
+  plugins: [react(), svgr()]
 });
