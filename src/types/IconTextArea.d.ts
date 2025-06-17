be74fed9e8e6c19e90d5226c9b8f493d1759863e
예은 <3_14km@naver.com>
2025-06-18 07:09:58 +0900

    refactor, feat: inputTextArea

---
+++ src/types/IconTextArea.d.ts
@@ -0,0 +1,20 @@
+export type TextAreaType = 'default' | 'fixed';
+
+export type DefaultTextAreaState = 'default' | 'typing' | 'complete' | 'error';
+export type FixedTextAreaState = 'default' | 'typing' | 'complete';
+
+export type TextAreaStateMap = {
+  default: DefaultTextAreaState;
+  fixed: FixedTextAreaState;
+};
+
+export interface BaseIconTextAreaProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  type?: TextAreaType;
+  state?: DefaultTextAreaState | FixedTextAreaState;
+  error?: boolean;
+  showIcon?: boolean;
+  typingMessage?: string;
+  useRegex?: boolean;
+  useLengthValidation?: boolean;
+  useTyping?: (isTyping: boolean) => void;
+}
