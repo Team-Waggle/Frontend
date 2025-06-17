76260ff594d3047098544af2acb8c413064ce192
예은 <3_14km@naver.com>
2025-06-18 02:41:50 +0900

    refactor, design: FormLabel
    InputBox에 쓰는 FormLabel

--- src/components/Profile/FormLabel.tsx
+++ src/components/Profile/FormLabel.tsx
@@ -1,14 +1,28 @@
+import RequiredIcon from '../../assets/inputBox/ic_input_required_red.svg?react'
+
 interface FormLabelProps {
-    children: React.ReactNode;
+    title: React.ReactNode;
+    caption?: React.ReactNode;
+    sideNote?: React.ReactNode;
+    isRequired?: boolean;
     className?: string;
-    secondaryText?: React.ReactNode;
   }
   
-  const FormLabel = ({ children, className, secondaryText }: FormLabelProps) => {
+  const FormLabel = ({ title, caption, sideNote, isRequired, className='' }: FormLabelProps) => {
     return (
-      <div className={`flex h-[21px] text-[14px] font-[600] items-center ${className ?? ''}`}>
-        {children}
-        {secondaryText && <div className="text-[12px] font-[500] ml-[14px]">{secondaryText}</div>}
+      <div className="flex flex-col items-start self-stretch text-black-130">
+        <div className="flex items-center gap-[0.8rem] self-stretch">
+          <div className="flex items-center gap-[0.2rem] text-subtitle-14_Sb600">
+              {title}
+              {isRequired && <RequiredIcon />}
+          </div>
+          <div className="flex pl-[0.6rem] items-center flex-grow flex-shrink-0 basis-0 self-stretch text-caption-12_M500 text-right">
+            {caption}
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-start self-stretch text-caption-13_M500">
+          {sideNote}
+        </div>
       </div>
     );
   };
