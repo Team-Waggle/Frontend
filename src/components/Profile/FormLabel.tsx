73b376cb7adbc538c05c5a85e5da4afa4e9448f3
Nam Min Ki <nam960216@gmail.com>
2025-07-26 19:09:43 +0900

    Revert "Merge branch 'main' into feat/main"
    This reverts commit 714cfea98f084b1863b7e6a7631c54e40cfafeea, reversing
    changes made to b85fdea5369d0b3d9ea696aab0e6fc13e511c901.

--- src/components/Profile/FormLabel.tsx
+++ src/components/Profile/FormLabel.tsx
@@ -1,14 +1,12 @@
 interface FormLabelProps {
     children: React.ReactNode;
     className?: string;
-    secondaryText?: React.ReactNode;
   }
   
-  const FormLabel = ({ children, className, secondaryText }: FormLabelProps) => {
+  const FormLabel = ({ children, className }: FormLabelProps) => {
     return (
       <div className={`flex h-[21px] text-[14px] font-[600] items-center ${className ?? ''}`}>
         {children}
-        {secondaryText && <div className="text-[12px] font-[500] ml-[14px]">{secondaryText}</div>}
       </div>
     );
   };
