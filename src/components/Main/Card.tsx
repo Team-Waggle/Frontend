4ec21422188711a130f3d0e5f5cd0c1ad9100936
Nam Min Ki <nam960216@gmail.com>
2025-08-06 22:40:13 +0900

    fix: Card 관련 작업

--- src/components/Main/Card.tsx
+++ src/components/Main/Card.tsx
@@ -1,51 +1,98 @@
+import dayjs from 'dayjs';
 import BaseTag from '../common/Tag/BaseTag';
-import BookmarkIcon from '../../assets/main/icon/icon-bookmark.svg?react';
-import Cinema_4DIcon from '../../assets/main/icon/skill/icon-skill-Cinema_4D.svg?react';
-import FigmaIcon from '../../assets/main/icon/skill/icon-skill-Figma.svg?react';
-import DjangoIcon from '../../assets/main/icon/skill/icon-skill-Django.svg?react';
-import JavascriptIcon from '../../assets/main/icon/skill/icon-skill-Javascript.svg?react';
-import MongoDBIcon from '../../assets/main/icon/skill/icon-skill-MongoDB.svg?react';
-import MeatballIcon from '../../assets/main/icon/icon-meatball.svg?react';
+import JavaIcon from '../../assets/icons/skill/large/ic_skill_Java_large.svg?react';
+import FigmaIcon from '../../assets/icons/skill/large/ic_skill_Figma_large.svg?react';
+import DjangoIcon from '../../assets/icons/skill/large/ic_skill_Django_large.svg?react';
+import JavascriptIcon from '../../assets/icons/skill/large/ic_skill_Javascript_large.svg?react';
+import MongoDBIcon from '../../assets/icons/skill/large/ic_skill_MongoDB_large.svg?react';
+import MeatballIcon from '../../assets/icons/skill/large/ic_skill_meatball_large.svg?react';
+import BookmarkWrapper from '../common/IconWrapper/BookmarkWrapper';
+import { CardData } from '../../types/card';
+import { useEffect, useState } from 'react';
 
-const Card = () => {
-  // domain, deadline, title, tags, skills, bookmark props로 받아오기
+interface CardProps {
+  data: CardData;
+}
+
+const Card = ({ data }: CardProps) => {
+  // 마감일 계산
+  const today = dayjs().startOf('day');
+  const end = dayjs(data?.recruitment_end_date).startOf('day');
+  const diff = end.diff(today, 'day');
+
+  const [isEnd, setIsEnd] = useState(false);
+
+  useEffect(() => {
+    if (diff < 0) setIsEnd(true);
+  }, []);
+
   return (
-    <div className="flex h-[21.5rem] w-[63rem] min-w-[32rem] max-w-[63rem] flex-col justify-between rounded-[2rem] border border-solid border-black-50 bg-black-10 px-[2.4rem] pb-[3rem] pt-[2rem] shadow-[0_0_0.2rem_0_#00000014]">
+    <div className="flex h-[21.5rem] w-[63rem] min-w-[32rem] max-w-[63rem] flex-col justify-between rounded-[2rem] border border-solid border-black-50 bg-black-10 px-[2.4rem] pb-[3rem] pt-[2rem]">
       <div className="flex h-[6.1rem] w-full flex-col gap-[1rem]">
         <div className="flex justify-between">
           {/* 도메인 이름 */}
-          <span className="h-[2.4rem] text-caption-13_M500 text-primary-70">
-            금융
+          <span
+            className={`h-[2.4rem] text-caption-13_M500 text-primary-70 ${isEnd ? 'text-opacity-30' : ''}`}
+          >
+            {data?.industry.display_name}
           </span>
           <div className="flex w-[9.5rem] gap-[1rem]">
-            <span className="w-[6.1rem] text-right text-body-13_R400 text-black-80">
-              D - 10
+            <span
+              className={`w-[6.1rem] text-right text-body-13_R400 ${isEnd ? 'text-black-80 text-opacity-30' : diff <= 7 ? 'text-error' : 'text-black-80'}`}
+            >
+              {diff > 0
+                ? `D - ${diff}`
+                : diff < 0
+                  ? `D + ${Math.abs(diff)}`
+                  : 'D - day'}
             </span>
-            {/* 북마크 아이콘은 기본으로 border가 1.5px임 f나 마감 상태일 때는 border 없애기 */}
-            <BookmarkIcon />
+            <BookmarkWrapper
+              projectId={data.id}
+              isBookmarked={data.bookmarked}
+              disabled={isEnd}
+            />
           </div>
         </div>
         {/* Title */}
-        <div className="text-title-18_Sb600">
-          [네오플] 게임그래픽 직군 분야별 모집 (근무지 : 서울)
+        <div
+          className={`text-title-18_Sb600 text-black-130 ${isEnd ? 'text-opacity-30' : ''}`}
+        >
+          {data?.title}
         </div>
       </div>
-      <div className="box-border flex h-[4.8rem] w-full gap-[0.8rem] whitespace-nowrap border-b border-solid border-black-40 pb-[1.6rem]">
-        <div className="flex items-center rounded-[0.4rem] bg-black-40 px-[1rem] text-subtitle-14_Sb600">
-          5/10명
-        </div>
+      <div
+        className={`flex h-[4.8rem] w-full gap-[0.8rem] whitespace-nowrap border-b border-solid border-black-40 pb-[1.6rem] ${isEnd ? 'opacity-30' : ''}`}
+      >
+        {data?.recruitments.map((data, idx) => (
+          <BaseTag
+            key={idx}
+            size="lg"
+            type="filled"
+            color="basic"
+            shape="square"
+          >
+            {data?.remaining_count}/{data?.current_count}명
+          </BaseTag>
+        ))}
         <div className="flex gap-[0.4rem]">
-          <BaseTag size="lg">프론트엔드</BaseTag>
-          <BaseTag size="lg">프론트엔드</BaseTag>
-          <BaseTag size="lg">프론트엔드</BaseTag>
-          <BaseTag size="lg">프론트엔드</BaseTag>
-          <BaseTag size="lg">프론트엔드</BaseTag>
-          <BaseTag size="lg">프론트엔드</BaseTag>
+          {data?.recruitments.map((data, idx) => (
+            <BaseTag
+              key={idx}
+              size="lg"
+              type="outline"
+              color="basic"
+              shape="square"
+            >
+              {data?.position.display_name}
+            </BaseTag>
+          ))}
         </div>
       </div>
-      {/* 스킬: 5개 초과 시 기타 아이콘 표시 */}
-      <div className="flex h-[2.4rem] w-[22rem] gap-[0.8rem]">
-        <Cinema_4DIcon />
+      {/* 스킬: 6개 이상 시 기타 아이콘 표시 */}
+      <div
+        className={`flex h-[2.4rem] w-[22rem] gap-[0.8rem] ${isEnd ? 'opacity-30' : ''}`}
+      >
+        <JavaIcon />
         <FigmaIcon />
         <DjangoIcon />
         <JavascriptIcon />
