c3a77d5a3e0445c720ec4d33a9436e14ab38bffc
Nam Min Ki <nam960216@gmail.com>
2025-08-18 17:21:39 +0900

    refactor: router 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './styles/global.css';
+import Header from './components/Header';
+import Main from './pages/Main';
+import Footer from './components/layout/Footer';
+import Layout from './components/layout/Layout';
+import NewApply from './pages/NewApply';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
-      <Outlet />
+      <Header />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/newapply" element={<NewApply />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/auth/callback" element={<Login />} />
+        </Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/routes.tsx';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import App from './App.tsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </StrictMode>,
+  // <StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
+  // </StrictMode>,
 );
