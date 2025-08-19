42bffc63b1a563811c16fc8dafae437f87ba29c2
예은 <143199798+b1uey0@users.noreply.github.com>
2025-08-20 02:24:59 +0900

    chore: SkillNameMap에서 사용하는 NestJS 명칭 수정

--- src/utils/skillNamemap.ts
+++ src/utils/skillNamemap.ts
@@ -5,7 +5,6 @@ export interface SkillInfo {
 
 export const skillNameMap: Record<string, SkillInfo> = {
   'C#': { iconKey: 'CSharp', displayName: 'C#' },
-  'Nestjs': { iconKey: 'Nestjs', displayName: 'Nest.js' },
   'Nextjs': { iconKey: 'Nextjs', displayName: 'Next.js' },
   'Nodejs': { iconKey: 'Nodejs', displayName: 'Node.js' },
 };
