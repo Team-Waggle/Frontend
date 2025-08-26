e4f90f4418ce6b60f0f859da2bd461d00acfeb3b
Nam Min Ki <nam960216@gmail.com>
2025-08-26 17:41:02 +0900

    fix: BookmarkWrapper api 연결

--- src/components/common/IconWrapper/BookmarkWrapper.tsx
+++ src/components/common/IconWrapper/BookmarkWrapper.tsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
+import { usePostBookmarkQuery } from '../../../hooks/useBookmark';
 import BookmarkIcon from '../../../assets/icons/nav/ic_nav_bookmark_small.svg?react';
 
 interface BookmarkWrapperProps {
-  isActive?: boolean;
+  projectId: number;
+  isBookmarked?: boolean;
   disabled?: boolean;
 }
 
-const BookmarkWrapper = ({ isActive, disabled }: BookmarkWrapperProps) => {
+const BookmarkWrapper = ({
+  projectId,
+  isBookmarked,
+  disabled,
+}: BookmarkWrapperProps) => {
+  const updateBookmark = usePostBookmarkQuery(projectId);
+  const [isActive, setIsActive] = useState(isBookmarked);
+
+  const handleClick = () => {
+    if (disabled) return;
+    updateBookmark.mutate();
+    setIsActive((prev) => !prev);
+  };
+
   return (
     <div
       className={`flex h-[2.4rem] w-[2.4rem] items-center justify-center ${
@@ -13,12 +29,15 @@ const BookmarkWrapper = ({ isActive, disabled }: BookmarkWrapperProps) => {
       }`}
     >
       <BookmarkIcon
+        onClick={handleClick}
         className={
           disabled
-            ? 'text-black-60'
+            ? isActive
+              ? 'fill-primary stroke-transparent opacity-30' // 마감 O 북마크 O
+              : 'fill-transparent stroke-black-70 opacity-30' // 마감 O 북마크 X
             : isActive
-              ? 'text-primary'
-              : 'fill-transparent stroke-black-60'
+              ? 'fill-primary stroke-transparent' // 마감 X 북마크 O
+              : 'fill-transparent stroke-black-70' // 마감 X 북마크 X
         }
       />
     </div>
