282706f94ea328992cc2921014dbf7666aa0ba1a
예은 <143199798+b1uey0@users.noreply.github.com>
2025-09-02 18:18:39 +0900

    refactor: InPageNavigation 색상 변경

--- src/components/common/InPageNavigation/InPageNavigation.tsx
+++ src/components/common/InPageNavigation/InPageNavigation.tsx
@@ -13,7 +13,7 @@ const InPageNavigation = ({ size="xl", items, isActive = false, onClick }: InPag
     <div
     onClick={onClick}
     className={`flex ${widthStyle} h-[4rem] min-w-[25rem] max-w-[26.4rem] px-[2.2rem] items-center flex-shrink-0 rounded-[6px]
-     ${isActive ? "bg-blue-100" : "hover:bg-black-30 active:bg-black-30"}`}>
+     ${isActive ? "bg-black-10" : "hover:bg-black-30 active:bg-black-30"}`}>
         <span className="text-black-130 text-body-14_M500"> {items} </span>
     </div>
   );
