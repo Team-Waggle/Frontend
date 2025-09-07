0ebcf2acd900263644b960c4d3305f75ecf840a3
Minki <nam960216@gmail.com>
2025-09-07 16:23:26 +0900

    design: button 디자인 수정

--- src/components/common/Button/styles.ts
+++ src/components/common/Button/styles.ts
@@ -1,6 +1,6 @@
 import { ButtonColor, ButtonSize } from '../../../types/button';
 
-export const BASE_BUTTON_STYLES = `flex justify-center items-center disabled:hover:bg-transparent`;
+export const BASE_BUTTON_STYLES = `flex justify-center items-center`;
 
 export const BUTTON_SIZE_STYLES: Record<ButtonSize, string> = {
   sm: 'h-[2.8rem] px-[1.6rem] text-caption-12_Sb600 rounded-[0.4rem] gap-[0.6rem]',
@@ -12,12 +12,12 @@ export const BUTTON_SIZE_STYLES: Record<ButtonSize, string> = {
 
 export const BUTTON_COLOR_STYLES: Record<ButtonColor, string> = {
   primary:
-    'bg-primary text-black-10 hover:bg-primary-80 disabled:bg-black-50 disabled:text-black-70',
+    'bg-primary text-black-10 sm:hover:bg-primary-80 active:bg-primary-80 disabled:bg-black-50 disabled:text-black-70',
   secondary:
-    'bg-primary-10 text-primary-80 hover:bg-primary-20 border border-solid border-primary-70',
-  line: 'bg-black-10 text-black-130 hover:bg-black-30 border border-solid border-black-60',
+    'bg-primary-10 text-primary-80 sm:hover:bg-primary-20 active:bg-primary-20 border border-solid border-primary-70 disabled:sm:hover:bg-primary-10',
+  line: 'bg-black-10 text-black-130 sm:hover:bg-black-30 active:bg-black-30 border border-solid border-black-60 disabled:sm:hover:bg-black-10',
   special:
-    'text-black-130 hover:bg-black-30 disabled:text-black-70 disabled:bg-text-70',
+    'bg-black-10 text-black-130 sm:hover:bg-black-30 active:bg-black-30 disabled:text-black-70 disabled:bg-text-70 disabled:sm:hover:bg-black-10',
   p_special:
-    'text-primary-70 hover:bg-black-30 disabled:text-black-70 disabled:bg-text-70',
+    'bg-black-10 text-primary-70 sm:hover:bg-black-30 active:bg-black-30 disabled:text-black-70 disabled:bg-text-70 disabled:sm:hover:bg-black-10',
 };
