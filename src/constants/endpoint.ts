ebd2ac9a1650037d2bcda5b7e2435b6935605102
예은 <143199798+b1uey0@users.noreply.github.com>
2025-09-25 00:23:14 +0900

    feat: Follow 기능 추가

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -10,6 +10,11 @@ export const USER_URL = (userId: string) => `/v1/users/${userId}`; // 특정 사
 export const USER_ME_URL = '/v1/users/me'; // 현재 사용자 조회, 수정, 삭제
 export const USER_PROFILE_IMAGE = '/v1/users/me/profile-image'; // 현재 사용자 이미지
 
+// 팔로우 API
+export const FOLLOW_URL = '/v1/follows' // 팔로우, 언팔로우 토글
+export const USER_FOLLOEES = '/v1/users/me/followees'
+export const USER_FOLLOWERS = '/v1/users/me/followers'
+
 // 프로젝트 모집 게시글 API
 export const PROJECTS_URL = '/v1/projects/post'; // 프로젝트 모집글 목록 조회, 생성
 export const PROJECTS_DETAIL_URL = (projectId: number) =>
