160f9f08c5f8f4b4bf85cbc6293a1aca5763a2a3
Minki <nam960216@gmail.com>
2025-10-10 21:45:31 +0900

    design: 모집글 작성 페이지 반응형 관련 수정

--- src/components/layout/Drawer.tsx
+++ src/components/layout/Drawer.tsx
@@ -8,13 +8,13 @@ children은 Drawer안에 Component를 넣어주면 됩니다.
 Drawer UI 예시
 
 모바일 화면에서 Drawer 보여주기
-  <div className="sm:hidden">
+  <div className="md:hidden">
     <Drawer>
       <SideFilters />
     </Drawer>
   </div>
 모바일 화면에서 Drawer 가리기
-  <div className="hidden sm:block">
+  <div className="hidden md:block">
     <SideFilters />
   </div> 
 */
@@ -32,7 +32,7 @@ const Drawer = ({ children }: DrawerProps) => {
       {isOpen && (
         <>
           <motion.div
-            className="fixed bottom-0 left-0 right-0 top-[7rem] z-40 bg-black/40 sm:hidden"
+            className="fixed bottom-0 left-0 right-0 top-[7rem] z-40 bg-black/40 md:hidden"
             onClick={close}
             // 오버레이가 서서히 나타났다 사라지는 애니메이션
             initial={{ opacity: 0 }}
@@ -42,7 +42,7 @@ const Drawer = ({ children }: DrawerProps) => {
           />
           {/* 오른쪽으로 슬라이드되는 드로어 애니메이션 */}
           <motion.aside
-            className="fixed left-0 top-[7rem] z-50 h-full w-[29rem] max-w-[30rem] bg-white sm:hidden"
+            className="fixed left-0 top-[7rem] z-50 h-full w-[29rem] max-w-[30rem] bg-white md:hidden"
             // 애니메이션 상태를 정의합니다.
             initial={{ x: '-100%' }} // 왼쪽 화면 밖에서 시작
             animate={{ x: 0 }} // 원래 위치(left-0)로 슬라이드
